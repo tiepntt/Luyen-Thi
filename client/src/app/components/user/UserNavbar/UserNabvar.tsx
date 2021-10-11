53b92788af428bf921320e39cb369a58692bfe7c
Nguyễn Thái Tiệp <44738396+NguyenThaiTiep@users.noreply.github.com>
2021-10-11 15:04:46 +0700

    init header and footer (#23)

---
+++ client/src/app/components/user/UserNavbar/UserNabvar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserNabvar = () => {
+  return <div></div>;
+};
+
+export default UserNabvar;
--- client/src/layouts/AppLayout/AppLayout.tsx
+++ client/src/layouts/AppLayout/AppLayout.tsx
@@ -1,6 +1,10 @@
 import { AppContext, AppModels } from "hooks/AppContext/AppContext";
 import React, { useState } from "react";
 
+import AppNavbar from "app/components/_share/Menu/AppNavbar/AppNavbar";
+import AppFooter from "app/components/_share/Footer/AppFooter/AppFooter";
+import "./style.scss";
+
 const AppLayout: React.FC = ({ children }) => {
   const [showHeader, setShowHeader] = useState(true);
   const [showFooter, setShowFooter] = useState(true);
@@ -10,11 +14,21 @@ const AppLayout: React.FC = ({ children }) => {
     setShowHeader,
     setShowFooter,
   };
+  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+
   return (
     <AppContext.Provider value={value}>
-      <div className="header"></div>
-      {children}
-      <div className="footer"></div>
+      <div id="app">
+        {showHeader && (
+          <div className="header">
+            <AppNavbar onMobileNavOpen={() => setMobileNavOpen(true)} />
+          </div>
+        )}
+        <div className="app-content">{children}</div>
+        <div className="footer">
+          <AppFooter />
+        </div>
+      </div>
     </AppContext.Provider>
   );
 };
