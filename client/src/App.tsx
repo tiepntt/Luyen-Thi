b5668dad1107936899debf247c00554cf1ba904f
Kien Duy <Kien Duy@KIENDUY>
2021-10-17 21:34:33 +0700

    add 404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import Routes from "app/pages/Routes";
 import AppLayout from "layouts/AppLayout/AppLayout";
 import React from "react";
 import { ToastContainer } from "react-toastify";
+import NotFoundPage from "./app/pages/404/NotFound";
+import { Redirect, Route, Switch, BrowserRouter } from "react-router-dom";
 import { Router } from "react-router";
 import "react-toastify/dist/ReactToastify.css";
 import { history } from "services/history";
@@ -10,35 +12,40 @@ import {
   ExtendedStringifyOptions,
   transformSearchStringJsonSafe,
 } from "use-query-params";
-import { Route } from "react-router-dom";
 const queryStringifyOptions: ExtendedStringifyOptions = {
   transformSearchString: transformSearchStringJsonSafe,
 };
 const App = () => {
   return (
-    <div className="app">
-      <Router history={history}>
-        <QueryParamProvider
-          ReactRouterRoute={Route}
-          stringifyOptions={queryStringifyOptions}
-        >
-          <AppLayout>
-            <Routes />
-          </AppLayout>
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-        </QueryParamProvider>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className="app">
+          <Router history={history}>
+            <QueryParamProvider
+              ReactRouterRoute={Route}
+              stringifyOptions={queryStringifyOptions}
+            >
+              <AppLayout>
+                <Routes />
+              </AppLayout>
+              <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+              />
+            </QueryParamProvider>
+          </Router>
+        </div>
+        <Route path="/404" component={NotFoundPage} />
+        <Redirect to="/404" />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
